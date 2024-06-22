e3936afd17aca74d4deebb0de37038a36bcfdbef
bpernot <pernot.benjamin@outlook.fr>
2024-06-22 10:00:31 +0200

    chore: update router with lazy loading and outlet

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,22 @@
-import React from "react"
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
-import GiftCardDetailPage from "./pages/GiftCardDetailPage/GiftCardDetailPage"
-import HomePage from "./pages/HomePage/HomePage"
+import React, { Suspense, lazy } from "react"
+import { Outlet, Route, BrowserRouter as Router, Routes } from "react-router-dom"
+
+const GiftCardDetailPage: React.LazyExoticComponent<React.FC<{}>> = lazy(() => import("./pages/GiftCardDetailPage/GiftCardDetailPage"))
+const HomePage: React.LazyExoticComponent<React.FC<{}>> = lazy(() => import("./pages/HomePage/HomePage"))
+
+const MainLayout = (): JSX.Element => <Outlet />
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/gift-card/:id" element={<GiftCardDetailPage />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="gift-card/:id" element={<GiftCardDetailPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </Router>
   )
 }
